0ceee81f05b8a9d617eb8e6b7b314254d15246e1
kevoru18 <kevoru@gmail.com>
2024-12-27 16:31:18 +0100

    Estoy pendiente con la paginacion de la tabla

--- mi_proyecto_web/app.py
+++ mi_proyecto_web/app.py
@@ -28,7 +28,7 @@ Este código está bajo la licencia MIT.
 from flask import Flask, render_template, request, redirect, session, url_for, flash, send_file, jsonify, send_from_directory, Blueprint,Response
 import time
 from flask_login import login_user, logout_user,login_required,LoginManager 
-
+import math
 from sqlalchemy import create_engine, text
 from flask_sqlalchemy import SQLAlchemy 
 import pandas as pd
@@ -213,8 +213,8 @@ def login():
       
          # Obtiene la contraseña enviada desde el formulario
         password = data.get('password')
-        # print('Username recibido:', username)
-        # # print('Username recibido:', password)
+        print('Username recibido:', username)
+        print('Username recibido:', password)
         # Validación del lado del servidor: verifica que el nombre de usuario tenga un formato válido
         # if not User.validate_username(username):
         #     # Si el formato no es válido, devuelve un mensaje de error en formato JSON
@@ -231,7 +231,7 @@ def login():
             usuario= user.usuario_windows
             ID_usuario = user.id
             ID_usuario_texto = str(ID_usuario)
-            # print('Username recibido:', nombre + ' ' + apellido1+ ' ' + usuario + ' ' + ID_usuario_texto)
+            print('Username recibido:', nombre + ' ' + apellido1+ ' ' + usuario + ' ' + ID_usuario_texto)
             # Actualiza la fecha y hora del último inicio de sesión
             # user.last_login = datetime.utcnow()
             # Guarda los cambios en la base de datos
@@ -239,13 +239,14 @@ def login():
             # Inicia la sesión del usuario utilizando Flask-Login
             # login_user(user)
             session['username']=username
-            
-            # print('Entro en login')
+            # clave_input=User.set_password(password,password)
+            # print('Password input', clave_input)
+            # # print('Entro en login')
                 # 2. Buscar en UsuariosApp (prueba) usando el ID del contacto
             usuario_app = UsuariosApp.query.filter_by(id=ID_usuario).first()
             # app_username = usuario_app.username
             if check_password_hash(usuario_app.password_hash, password):
-                # print('Password correcta', usuario_app.password_hash)
+                print('Password correcta', usuario_app.password_hash)
                 # Hash and update password
                 # password_hash = set_password(new_password)
                 # Renderiza la plantilla 'inicio.html' en caso de inicio de sesión exitoso
@@ -566,167 +567,168 @@ def limpiar_telefono(telefono):
     telefono = re.sub(r'^(\+34|0034)', '', telefono)
     return telefono if len(telefono) == 9 and telefono.isdigit() else None
 
-progress = 0
+# progress = 0
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if "username" in session:
-        global progress
-        # nuevo
-        progress = 0  # Reiniciar progreso
-        total_steps = 100
-   
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(url_for('index'))
+    if "username" not in session:
+        return render_template('login.html')
+    # global progress
+    # # nuevo
+    # progress = 0  # Reiniciar progreso
+    # total_steps = 100
 
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(url_for('index'))
+    if 'file' not in request.files or request.files['file'].filename == '':
+        flash('Archivo no válido')
+        return redirect(url_for('index'))
 
-        if file:
-            filepath = os.path.join(UPLOAD_FOLDER, file.filename)
-            file.save(filepath)
 
+    file = request.files['file']
+    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(filepath)
+    # if file:
+    try:
         # Procesar el archivo Excel
-            df = pd.read_excel(filepath)
-
+        df = pd.read_excel(filepath)
         # Loguear los datos iniciales
-            registros_originales = len(df)
-            nombre_usuario = getpass.getuser()  # Obtener el nombre del usuario logueado
-
+        registros_originales = len(df)
+        nombre_usuario = getpass.getuser()  # Obtener el nombre del usuario logueado
         # Renombrar las columnas a un formato estándar
-            df.rename(columns=lambda x: x.strip().lower(), inplace=True)
-            df.rename(columns={
-                'telefono': 'telefono',
-                'teléfono': 'telefono',
-                'telf1': 'telefono',
-                'tel1': 'telefono',
-                'telefono1': 'telefono',
-                'TELEFONO1': 'telefono',
-                'TELEFONO': 'telefono',
-                'phone': 'telefono'
-            }, inplace=True)
+        column_mapping={
+            'telefono': 'telefono',
+            'teléfono': 'telefono',
+            'telf1': 'telefono',
+            'tel1': 'telefono',
+            'telefono1': 'telefono',
+            'TELEFONO1': 'telefono',
+            'TELEFONO': 'telefono',
+            'phone': 'telefono'
+        }
+        df.rename(columns=lambda x: x.strip().lower(), inplace=True)
+        df.rename(columns=column_mapping, inplace=True)
 
-        # Verificar si 'telefono' existe
-            if 'telefono' not in df.columns:
-                raise KeyError("No se encontró la columna 'telefono' en el DataFrame.")
+    # Verificar si 'telefono' existe
+        if 'telefono' not in df.columns:
+            raise KeyError("No se encontró la columna 'telefono' en el archivo.")
 
-        # Limpiar los teléfonos
-            total_steps = len(df) + 3  # Procesos principales + registros
-            df['telefono_limpio'] = df['telefono'].apply(limpiar_telefono)
-            registros_limpios = df['telefono_limpio'].notnull().sum()
+    # Limpiar los teléfonos
+        # total_steps = len(df) + 3  # Procesos principales + registros
+        df['telefono_limpio'] = df['telefono'].apply(limpiar_telefono)
+        registros_limpios = df['telefono_limpio'].notnull().sum()
+  # Registrar la actividad en el log
+        log_usuario(nombre_usuario, file.filename, registros_originales, registros_limpios)
 
- # Simulación del progreso: Renombrar columnas
-            progress = int((1 / total_steps) * 100)
-            time.sleep(0.5)  # Simular demora
-        # Registrar la actividad en el log
-            log_usuario(nombre_usuario, file.filename, registros_originales, registros_limpios)
+    # Conectar a la base de datos y ejecutar la lógica de scoring
+        with engine.connect() as conn:
+            # total_steps = 3  # Número total de pasos
+            # current_step = 1
+        # send_progress_update(current_step, total_steps)
+            for index, row in df.iterrows():
+                telefono = str(row['telefono'])
 
-        # Conectar a la base de datos y ejecutar la lógica de scoring
-            with engine.connect() as conn:
-                total_steps = 3  # Número total de pasos
-                current_step = 1
-            # send_progress_update(current_step, total_steps)
-                for index, row in df.iterrows():
-                    telefono = str(row['telefono'])
-
-                # Consulta de scoring
-                    query_scoring = text("""
-                        SELECT CASE 
-                            WHEN COUNT(*) >= 20 THEN (CAST(SUM(CASE WHEN CallStatusNum < 11 THEN 1 ELSE 0 END) AS FLOAT) / COUNT(*)) * 100 
-                            ELSE (SUM(CASE WHEN CallStatusNum < 11 THEN 1 ELSE 0 END) / 20.0) * 100 
-                        END AS conteo 
-                        FROM ODCalls WHERE ANI = :telefono
-                    """)
-                    result_scoring = conn.execute(query_scoring, {'telefono': telefono}).fetchone()
-                    conteo = result_scoring[0] if result_scoring else 0
-                    df.at[index, 'scoring'] = conteo
-
-                # Consulta de media de intentos para contacto
-                    query_media_intentos = text("""
-                        SELECT CASE 
-                            WHEN COUNT(*) >= 20 THEN (COUNT(*) / CAST(NULLIF(SUM(CASE WHEN CallStatusNum < 11 THEN 1 ELSE 0 END), 0) AS FLOAT)) 
-                            ELSE (20.0 / NULLIF(SUM(CASE WHEN CallStatusNum < 11 THEN 1 ELSE 0 END), 0))  
-                        END AS conteo 
-                        FROM ODCalls 
-                        WHERE ANI = :telefono AND Duration > 1
-                    """)
-
-                    result_media_intentos = conn.execute(query_media_intentos, {'telefono': telefono}).fetchone()
-                    media_intentos = result_media_intentos[0] if result_media_intentos else None
-                    df.at[index, 'media_intentos_para_contacto'] = media_intentos
-
-                # Consulta de media de intentos para contacto positivo
-                    query_media_intentos_positivo = text("""
-                        SELECT 
-                            CASE 
-                                WHEN COUNT(CASE WHEN Duration > 1 AND CallStatusNum BETWEEN 1 AND 10 THEN 1 END) = 0 THEN 0
-                                ELSE COUNT(*) * 1.0 / COUNT(CASE WHEN Duration > 1 AND CallStatusNum BETWEEN 1 AND 10 THEN 1 END)
-                            END AS media_llamadas_para_contacto_positivo
-                        FROM ODCalls
-                        WHERE ANI = :telefono AND (Duration > 1 OR CallStatusNum BETWEEN 1 AND 10)
-                    """)
-
-                    result_media_intentos_positivo = conn.execute(query_media_intentos_positivo, {'telefono': telefono}).fetchone()
-                    # Actualizar el progreso
-                    progress = int(((index + 2) / total_steps) * 100)
-                    time.sleep(0.1)  # Simular tiempo de procesamiento
-                    current_step += 1
-                # send_progress_update(current_step, total_steps)
-
+            # Consulta de scoring
+                query_scoring = text("""
+                    SELECT CASE 
+                        WHEN COUNT(*) >= 20 THEN (CAST(SUM(CASE WHEN CallStatusNum < 11 THEN 1 ELSE 0 END) AS FLOAT) / COUNT(*)) * 100 
+                        ELSE (SUM(CASE WHEN CallStatusNum < 11 THEN 1 ELSE 0 END) / 20.0) * 100 
+                    END AS conteo 
+                    FROM ODCalls WHERE ANI = :telefono
+                """)
+                result_scoring = conn.execute(query_scoring, {'telefono': telefono}).fetchone()
+                # nuevo comentario
+                # conteo = result_scoring[0] if result_scoring else 0
+                # df.at[index, 'scoring'] = conteo
                 
-                
+                df.at[index, 'scoring'] = result_scoring[0] if result_scoring else 0
+
+            # Consulta de media de intentos para contacto
+                query_media_intentos = text("""
+                    SELECT CASE 
+                        WHEN COUNT(*) >= 20 THEN (COUNT(*) / CAST(NULLIF(SUM(CASE WHEN CallStatusNum < 11 THEN 1 ELSE 0 END), 0) AS FLOAT)) 
+                        ELSE (20.0 / NULLIF(SUM(CASE WHEN CallStatusNum < 11 THEN 1 ELSE 0 END), 0))  
+                    END AS conteo 
+                    FROM ODCalls 
+                    WHERE ANI = :telefono AND Duration > 1
+                """)
+
+                result_media_intentos = conn.execute(query_media_intentos, {'telefono': telefono}).fetchone()
+                df.at[index, 'media_intentos_para_contacto'] = result_media_intentos[0] if result_media_intentos else None
+
+            # Consulta de media de intentos para contacto positivo
+                query_media_intentos_positivo = text("""
+                    SELECT 
+                        CASE 
+                            WHEN COUNT(CASE WHEN Duration > 1 AND CallStatusNum BETWEEN 1 AND 10 THEN 1 END) = 0 THEN 0
+                            ELSE COUNT(*) * 1.0 / COUNT(CASE WHEN Duration > 1 AND CallStatusNum BETWEEN 1 AND 10 THEN 1 END)
+                        END AS media_llamadas_para_contacto_positivo
+                    FROM ODCalls
+                    WHERE ANI = :telefono AND (Duration > 1 OR CallStatusNum BETWEEN 1 AND 10)
+                """)
+
+                result_media_intentos_positivo = conn.execute(query_media_intentos_positivo, {'telefono': telefono}).fetchone()
                 # Agregar un log para depuración
-                    if result_media_intentos_positivo is None:
-                        logging.info(f'No se encontraron resultados para el teléfono: {telefono}')
-                #else:
-                    #logging.info(f'Resultado para el teléfono {telefono}: {result_media_intentos_positivo[0]}')
+                if result_media_intentos_positivo is None:
+                    logging.info(f'No se encontraron resultados para el teléfono: {telefono}')
+      
+                media_intentos_positivo = result_media_intentos_positivo[0] if result_media_intentos_positivo else None
+                df.at[index, 'media_intentos_para_contacto_positivo'] = media_intentos_positivo
 
-                    media_intentos_positivo = result_media_intentos_positivo[0] if result_media_intentos_positivo else None
-                    df.at[index, 'media_intentos_para_contacto_positivo'] = media_intentos_positivo
+    # Ordenar el DataFrame de mayor a menor según el scoring
+        df.sort_values(by='scoring', ascending=False, inplace=True)
+    # Limitar a los primeros 5 registros
+        df = df.head(registros_originales)
+        page = request.args.get("page", 1, type=int)
+        
+        
+        
+    # # queda pendiente paginacion
+    #     rows_per_page = 5
+    #     total_pages = math.ceil(len(data) / rows_per_page)
+    #     start = (page - 1) * rows_per_page
+    #     end = start + rows_per_page
+    #     page_data = data[start:end]
 
-        # Ordenar el DataFrame de mayor a menor según el scoring
-            df.sort_values(by='scoring', ascending=False, inplace=True)
-        # Limitar a los primeros 5 registros
-            df = df.head(5)
 
-        # Convertir los valores numéricos pequeños a un formato más legible con 2 decimales
-            df['media_intentos_para_contacto_positivo'] = df['media_intentos_para_contacto_positivo'].apply(lambda x: '{:.2f}'.format(x) if pd.notna(x) else x)
 
-        # Guardar el archivo Excel con el scoring, media de intentos y todos los datos
-            output_filepath = os.path.join(UPLOAD_FOLDER, 'resultados_Scoring.xlsx')
-            df.to_excel(output_filepath, index=False)
 
-        # Aplicar estilo de colores en la columna 'scoring' usando openpyxl
-            wb = load_workbook(output_filepath)
-            ws = wb.active
-            scoring_column_index = ws.max_column - 2  # Columna 'scoring'
-            media_intentos_column_index = ws.max_column - 1  # Columna 'media_intentos_para_contacto'
-            media_intentos_positivo_column_index = ws.max_column  # Última columna es 'media_intentos_para_contacto_positivo'
 
-            for row in range(2, ws.max_row + 1):  # Empezamos en la fila 2 para evitar el encabezado
-                score = ws.cell(row=row, column=scoring_column_index).value
-                if score is not None:
-                # Definir el color basado en el score (de verde a rojo)
-                    red = int(min(255, (255 - int(2.55 * score))))
-                    green = int(min(255, int(2.55 * score)))
-                    fill = PatternFill(start_color=f"{red:02X}{green:02X}00", end_color=f"{red:02X}{green:02X}00", fill_type="solid")
-                
-                    ws.cell(row=row, column=scoring_column_index).fill = fill
 
-            wb.save(output_filepath)
-            wb.close()
+    # Convertir los valores numéricos pequeños a un formato más legible con 2 decimales
+        df['media_intentos_para_contacto_positivo'] = df['media_intentos_para_contacto_positivo'].apply(lambda x: '{:.2f}'.format(x) if pd.notna(x) else x)
 
-        # Generar la tabla HTML para mostrar en el navegador
-            resultados_limpieza = df.to_html(classes=['table', 'scoring'], justify='center')
-          # Finalizar el progreso
-            progress = 100
-            time.sleep(0.5)
+    # Guardar el archivo Excel con el scoring, media de intentos y todos los datos
+        output_filepath = os.path.join(UPLOAD_FOLDER, 'resultados_Scoring.xlsx')
+        df.to_excel(output_filepath, index=False)
 
-            return render_template('resultados.html', tabla=resultados_limpieza) #jsonify({'progress': 100, 'results': resultados_limpieza})             #      #, 
-    return render_template('login.html')
+    # Aplicar estilo de colores en la columna 'scoring' usando openpyxl
+        wb = load_workbook(output_filepath)
+        ws = wb.active
+        scoring_column_index = ws.max_column - 2  # Columna 'scoring'
+        # media_intentos_column_index = ws.max_column - 1  # Columna 'media_intentos_para_contacto'
+        # media_intentos_positivo_column_index = ws.max_column  # Última columna es 'media_intentos_para_contacto_positivo'
+
+        for row in range(2, ws.max_row + 1):  # Empezamos en la fila 2 para evitar el encabezado
+            score = ws.cell(row=row, column=scoring_column_index).value
+            if score is not None:
+            # Definir el color basado en el score (de verde a rojo)
+                red = int(min(255, (255 - int(2.55 * score))))
+                green = int(min(255, int(2.55 * score)))
+                fill = PatternFill(start_color=f"{red:02X}{green:02X}00", end_color=f"{red:02X}{green:02X}00", fill_type="solid")
+            
+                ws.cell(row=row, column=scoring_column_index).fill = fill
+
+        wb.save(output_filepath)
+        wb.close()
+
+    # Generar la tabla HTML para mostrar en el navegador
+        resultados_limpieza = df.to_html(classes=['table', 'scoring'], justify='center')
+        return render_template('resultados.html', tabla=resultados_limpieza) #jsonify({'progress': 100, 'results': resultados_limpieza})             #      #, 
+
     
+    except Exception as e:
+        logging.error(f"Error processing file: {e}")
+        flash('Error processing file')
+        return redirect(url_for('index'))
+
     
     
     
